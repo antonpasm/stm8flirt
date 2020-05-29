a46fbab4d3504424d838b244c7da7e3ae2964e01
pasm <antonpasm@gmail.com>
2020-05-29 16:47:26 +0300

    Варианты компиляции StdPeriph

--- Compile_STM8_StdPeriph/Compile.cmd
+++ Compile_STM8_StdPeriph/Compile.cmd
@@ -1,12 +1,14 @@
+set device=STM8L15X_HD
+
 call :compile medium medium hz mm_hz
 ::call :compile small small hz ss_hz
 
 @goto :eof
 
+
 :compile
 @set icc="C:\Program Files (x86)\IAR Systems\Embedded Workbench 6.5\stm8\bin\iccstm8.exe"
 @set iarc="C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\stm8\bin\iarchive.exe"
-@set def=-D STM8L15X_HD
 @set cmod=%1
 @set dmod=%2
 @set opt=%3
@@ -15,7 +17,7 @@ call :compile medium medium hz mm_hz
 @set obj=obj_%suff%
 
 @mkdir %obj% 2>NUL
-@for %%f in (%stm8lib%\src\*.c) do %icc% %%~f -e -O%opt% --code_model %cmod% --data_model %dmod% %def% -o %obj%\ -I %stm8lib%\inc -I .\
+@for %%f in (%stm8lib%\src\*.c) do %icc% %%~f -e -O%opt% --code_model %cmod% --data_model %dmod% -D %device% -o %obj%\ -I %stm8lib%\inc -I .\
 
 @setlocal EnableDelayedExpansion
 @pushd %cd%
