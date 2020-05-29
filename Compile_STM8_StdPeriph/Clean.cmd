ce188e64cf8328b073b008d8bc9f3a264e77164e
pasm <antonpasm@gmail.com>
2020-05-29 16:26:44 +0300

    Варианты компиляции StdPeriph

--- Compile_STM8_StdPeriph/Clean.cmd
+++ Compile_STM8_StdPeriph/Clean.cmd
@@ -1,2 +1,2 @@
-@set obj=obj
-rmdir /S /Q .\%obj%
+@set obj=obj_*
+@for /d %%d in (%obj%) do rmdir /S /Q %%~d
--- Compile_STM8_StdPeriph/Compile.cmd
+++ Compile_STM8_StdPeriph/Compile.cmd
@@ -1,16 +1,26 @@
+call :compile medium medium hz mm_hz
+:call :compile small small hz ss_hz
+
+@goto :eof
+
+:compile
 @set icc="C:\Program Files (x86)\IAR Systems\Embedded Workbench 6.5\stm8\bin\iccstm8.exe"
 @set iarc="C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\stm8\bin\iarchive.exe"
 @set def=-D STM8L15X_HD
-@set mod=--code_model medium --data_model medium
+@set cmod=%1
+@set dmod=%2
+@set opt=%3
+@set suff=%4
 @set stm8lib=STM8L15x_StdPeriph_Driver
-@set obj=obj
+@set obj=obj_%suff%
 
-@mkdir %obj%
-@for %%f in (%stm8lib%\src\*.c) do %icc% %%~f -e -Ohz %mod% %def% -o %obj%\ -I %stm8lib%\inc -I .\
+@mkdir %obj% 2>NUL
+@for %%f in (%stm8lib%\src\*.c) do %icc% %%~f -e -O%opt% --code_model %cmod% --data_model %dmod% %def% -o %obj%\ -I %stm8lib%\inc -I .\
 
 @setlocal EnableDelayedExpansion
 @pushd %cd%
 @cd %obj%
 @for %%f in (*.o) do @set list=!list! %%~f
-%iarc% --create %stm8lib%.a %list%
+%iarc% --create %stm8lib%_%suff%.a %list%
 @popd
+@exit /b
